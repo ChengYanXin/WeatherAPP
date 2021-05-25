551805e5987f284c7aab4f40d63fe7026df47fd3
chengyanxin <chengyanxin@mingyuanyun.com>
2021-05-25 14:13:55 +0800

    第一次提交

---
+++ WebWeatherAPP.Core/Controllers/WeatherController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebWeatherAPP.Core.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "程艳新";
+        }
+    }
+}
