eb31bbd9f948a485c8d0fe540d1d2bc4efbe6776
chengyanxin <chengyanxin@mingyuanyun.com>
2021-05-25 15:53:31 +0800

    调用外部Api返回天气数据/添加HttpHelper帮助类

--- WebWeatherAPP.Core/Controllers/WeatherController.cs
+++ WebWeatherAPP.Core/Controllers/WeatherController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using WeatherAPP.Comman;
 
 namespace WebWeatherAPP.Core.Controllers
 {
@@ -11,10 +14,25 @@ namespace WebWeatherAPP.Core.Controllers
     [ApiController]
     public class WeatherController : ControllerBase
     {
+        /// <summary>
+        /// 调用外部api返回数据
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public string Get()
+        public string GetWeather()
         {
-            return "程艳新";
+            
+            try
+            {
+                //Key需要从https://dev.qweather.com/docs/api/weather/weather-daily-forecast/获取
+                var jsonResult =HttpHelper.Get($"https://devapi.qweather.com/v7/weather/3d?location=101281701&key=b2a7474bbef1463bb9f4beca63fee6bf");
+                return jsonResult;
+            }
+            catch (Exception ex)
+            {
+
+                throw (ex);
+            }
         }
     }
 }
